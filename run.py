02993ab49c5388162601daaf3788cf4b72ff9583
kanchihari <harishkanchi.rishi@gmail.com>
2023-09-18 19:34:12 +0200

    Add item display and user input functions

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from datetime import datetime
+
+# Function to display the available items
+def display_item_list():
+    print("Available Items:")
+    print(lists)  # It seems like `lists` should be defined somewhere; it's not in the provided code
+
+# Function to get the user's item choice
+def get_user_item_choice():
+    while True:
+        item = input("Enter the item you want to buy: ")
+        if item in items:  # `items` is not defined; this will cause an error
+            return item
+        else:
+            print("Invalid item! Please enter a valid item.")
+
+# It would be beneficial to define and initialize 'lists' and 'items' somewhere in the code
+
