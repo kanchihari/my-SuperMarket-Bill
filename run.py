21baec56c57ef7b22801d5f5feebfcf669c0d2f1
kanchihari <harishkanchi.rishi@gmail.com>
2023-09-18 19:38:17 +0200

    Implement user input for item quantity

--- run.py
+++ run.py
@@ -3,16 +3,28 @@ from datetime import datetime
 # Function to display the available items
 def display_item_list():
     print("Available Items:")
-    print(lists)  # It seems like `lists` should be defined somewhere; it's not in the provided code
+    print(lists)  
 
 # Function to get the user's item choice
 def get_user_item_choice():
     while True:
         item = input("Enter the item you want to buy: ")
-        if item in items:  # `items` is not defined; this will cause an error
+        if item in items: 
             return item
         else:
             print("Invalid item! Please enter a valid item.")
+# Function to get the user's desired quantity for the item
+def get_user_item_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter the quantity you want to buy: "))  
+            if quantity > 0:  
+                return quantity
+            else:
+                print("Invalid quantity! Please enter a valid quantity.")  
+        except ValueError:
+            print("Invalid input! Please enter a valid numeric quantity.")  
 
-# It would be beneficial to define and initialize 'lists' and 'items' somewhere in the code
+
+
 
